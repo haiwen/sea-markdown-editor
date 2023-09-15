51b6d7acc74958ae4803e60bd77fcc79a5dd34ff
杨顺强 <978987373@qq.com>
2023-09-15 10:59:46 +0800

    optimize code

--- site/index.js
+++ site/index.js
@@ -1,20 +1,21 @@
 import React, { Suspense } from 'react';
 import ReactDom from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
 import Loading from './commons/loading';
 import i18n from './_i18n';
-import SeafileEditor from './pages/seafile-editor';
+import App from './app';
 
-import './assets/css/reset.css';
-
-export default function App() {
+export default function Index() {
   return (
     <I18nextProvider i18n={i18n}>
-      <Suspense fallback={<Loading />}>
-        <SeafileEditor />
-      </Suspense>
+      <BrowserRouter>
+        <Suspense fallback={<Loading />}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
     </I18nextProvider>
   );
 }
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<Index />, document.getElementById('root'));
---
+++ site/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink} from 'react-router-dom';
+
+import '../assets/css/app.css';
+
+export default function Home() {
+  return (
+    <div className='app'>
+      <div className='header'>seafile editor 测试页面</div>
+      <div className='nav-container'>
+        <NavLink to={''}></NavLink>
+        <NavLink to={'/seafile-editor'}>Seafile Editor</NavLink>
+        <NavLink to={'/seafile-viewer'}>Seafile Viewer</NavLink>
+        <NavLink to={'/wiki-view'}>Wiki Viewer</NavLink>
+      </div>
+      <div className='tip-message'>请点击其他链接，测试不同的页面内容</div>
+    </div>
+  );
+}
