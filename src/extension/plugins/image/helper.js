978552392ee72e3215c364a4d7caa8c6f334ea03
liuhongbo <916196375@qq.com>
2023-09-20 10:28:18 +0800

    feat: test reabse

--- src/extension/plugins/image/helper.js
+++ src/extension/plugins/image/helper.js
@@ -4,5 +4,8 @@ export const isMenuDisabled = ( editor, readonly ) => {
   // const { selection } = editor;
   if (readonly) return true;
   // todo 需要补充判断光标不在段落时，不可用的逻辑（刷新页面后Editor会失焦，这个就是想做的）
+  const [match] = Editor.nodes(editor, {
+    match: n => n.type === 'paragraph',
+  });
   return false;
 };
