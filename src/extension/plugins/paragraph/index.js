9c61d342c8f37fd8231bdb95a4be6bd7d59a8a33
liuhongbo <916196375@qq.com>
2023-10-20 11:31:52 +0800

    refactor: move check-list logic from paragrph to check-list

--- src/extension/plugins/paragraph/index.js
+++ src/extension/plugins/paragraph/index.js
@@ -1,11 +1,9 @@
 import { PARAGRAPH } from '../../constants/element-types';
-import withParagraph from './plugin';
 import renderParagraph from './render-elem';
 
 const ParagraphPlugin = {
   type: PARAGRAPH,
   nodeType: 'element',
-  editorPlugin: withParagraph,
   renderElements: [renderParagraph]
 };
 
