c407eea2f0d5292427fc0661a5bbe2890be77df1
杨顺强 <978987373@qq.com>
2023-11-29 14:56:19 +0800

    optimize code

---
+++ tests/slate-convert/md-to-html/image_reference.test.js
@@ -0,0 +1,17 @@
+import processor from '../../../src/slate-convert/md-to-html';
+
+const mdString = '![Alt text][logo]\n\n[logo]: /logo.png "title"';
+
+describe('image test', () => {
+  it('paragraph > image', async () => {
+    const result = await processor.process(mdString);
+    const string = String(result);
+    const expectResult = `
+<p>
+  <img src="/logo.png" alt="Alt text" title="title">
+</p>
+`;
+
+    expect(string).toEqual(expectResult);
+  });
+});
