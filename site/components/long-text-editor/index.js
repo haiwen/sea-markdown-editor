dbda3a6ff77b69fea7dfb7dcc832cee4f7ca30b4
杨顺强 <978987373@qq.com>
2024-02-28 11:13:47 +0800

    add long-text test

---
+++ site/components/long-text-editor/index.js
@@ -0,0 +1,61 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { SimpleEditor } from '@seafile/seafile-editor';
+
+import './style.css';
+import LongTextModal from './longtext-modal';
+
+export default function LongTextEditor({ readOnly, headerName, value, updateValue, onCloseEditor }) {
+  const editorRef = useRef(null);
+  const [isValueChanged, setValueChanged] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [dialogStyle, setDialogStyle] = useState({});
+
+  const onContentChanged = useCallback(() => {
+    setValueChanged(true);
+  }, []);
+
+  const onCloseToggle = useCallback(() => {
+    const value = editorRef.current.getValue();
+    console.log(isValueChanged);
+    if (isValueChanged) {
+      updateValue(value);
+    }
+    onCloseEditor();
+  }, [isValueChanged, onCloseEditor, updateValue]);
+
+  const onFullScreenToggle = useCallback(() => {
+    setIsFullScreen(!isFullScreen);
+  }, [isFullScreen]);
+
+  const onContainerKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      event.preventDefault();
+      event.stopPropagation();
+      onCloseToggle();
+    }
+  };
+
+  return (
+    <LongTextModal onModalClick={onCloseToggle}>
+      <div style={dialogStyle} className="longtext-modal-dialog">
+        <div className={`longtext-modal-dialog-header ${readOnly ? 'longtext-modal-dialog-header-border' : ''}`}>
+          <div className="longtext-header">
+            <span className="longtext-header-name">{headerName}</span>
+            <div className="longtext-header-tool">
+              <span onClick={onFullScreenToggle} className={`mr-1 longtext-header-tool-item dtable-font dtable-icon-full-screen ${isFullScreen ? 'long-text-full-screen' : ''}`}></span>
+              <span onClick={onCloseToggle} className="longtext-header-tool-item dtable-font dtable-icon-x"></span>
+            </div>
+          </div>
+        </div>
+        <div onKeyDown={onContainerKeyDown} className={`longtext-container ${readOnly ? 'longtext-container-scroll' : ''}`}>
+          <SimpleEditor
+            ref={editorRef}
+            value={value}
+            onContentChanged={onContentChanged}
+          />
+        </div>
+      </div>
+    </LongTextModal>
+  );
+}
+
---
+++ site/components/long-text-editor/longtext-modal.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  containerClass: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  onModalClick: PropTypes.func,
+};
+
+class LongTextModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const { containerClass } = this.props;
+    this.el = document.createElement('div');
+    this.el.className = 'longtext-modal';
+    this.el.className = `longtext-modal ${containerClass}`;
+    document.body.appendChild(this.el);
+  }
+
+  componentDidMount() {
+    this.el.addEventListener('mousedown', this.onModalClick);
+  }
+
+  componentWillUnmount() {
+    this.el.removeEventListener('mousedown', this.onModalClick);
+    document.body.removeChild(this.el);
+  }
+
+  onModalClick = (e) => {
+    e && e.stopPropagation();
+    const className = e.target.className;
+    if (typeof className !== 'string') return;
+    if (this.props.onModalClick && (className === 'longtext-modal' || className.startsWith('longtext-modal') )) {
+      this.props.onModalClick();
+    }
+  };
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
+    );
+  }
+}
+
+LongTextModal.propTypes = propTypes;
+export default LongTextModal;
