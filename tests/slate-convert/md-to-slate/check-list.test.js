ee5aceb4920e781d8d4fe465e33cbbd6c87a4628
杨顺强 <978987373@qq.com>
2024-03-15 14:04:09 +0800

    update version

--- tests/slate-convert/md-to-slate/check-list.test.js
+++ tests/slate-convert/md-to-slate/check-list.test.js
@@ -32,3 +32,56 @@ describe('check list test', () => {
     expect(formatChildren(nodes)).toEqual(expectResult);
   });
 });
+
+describe('check list test with child list', () => {
+  it('check list', () => {
+    const mdString = '- [x] Write the press release \n    - [x] Write the press release \n    - [ ] Update the website \n    - [ ] Contact the media\n- [ ] Update the website \n- [ ] Contact the media';
+    const nodes = mdStringToSlate(mdString);
+    const expectResult = [
+      {
+        type: 'check_list_item',
+        checked: true,
+        children: [
+          { text: 'Write the press release' },
+        ]
+      },
+      {
+        type: 'check_list_item',
+        checked: true,
+        children: [
+          { text: 'Write the press release' },
+        ]
+      },
+      {
+        type: 'check_list_item',
+        checked: false,
+        children: [
+          { text: 'Update the website' },
+        ]
+      },
+      {
+        type: 'check_list_item',
+        checked: false,
+        children: [
+          { text: 'Contact the media' },
+        ]
+      },
+      {
+        type: 'check_list_item',
+        checked: false,
+        children: [
+          { text: 'Update the website' },
+        ]
+      },
+      {
+        type: 'check_list_item',
+        checked: false,
+        children: [
+          { text: 'Contact the media' },
+        ]
+      },
+    ];
+
+    expect(formatChildren(nodes)).toEqual(expectResult);
+  });
+});
