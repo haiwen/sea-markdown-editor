fa2c634605d28e11a28dd796ae189036f1e92a52
杨顺强 <978987373@qq.com>
2024-03-16 15:36:17 +0800

    fix wraning

--- src/extension/plugins/header/menu/index.js
+++ src/extension/plugins/header/menu/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, Fragment, useMemo, useCallback } from 'react';
+import React, { useState, useRef, Fragment, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { useTranslation } from 'react-i18next';
