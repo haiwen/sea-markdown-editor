69a59e4788231f20c38b0d66a09a590909d6235f
杨顺强 <978987373@qq.com>
2024-05-08 13:51:26 +0800

    update setting

--- site/setting.local.dist.js
+++ site/setting.local.dist.js
@@ -1,23 +1,15 @@
 const config = {
-  serviceUrl: 'http://127.0.0.1', // required
-  name: '**', // required, current user's name
-  username: '**', // required
-  password: '**', // required
-  avatarURL: '**', // required, current user's avatar_url
+  serviceUrl: 'https://dev.seafile.com/seahub',
+  username: '**',
+  password: '**',
+  name: '**',
   contact_email: '**',
   lib_name: '**',
-  repoID: '8c911722-6a67-420d-a52d-aa6e8dcd6279', // required
-  dirPath: '/', // required
-  docPath: '/aaa.sdoc',  // required
-  docName: 'aaa.sdoc', // required
-  docUuid: '66d73184-4a41-4ee9-9e12-3a64c3527fbb', // required
-  sdocServer: 'http://127.0.0.1:7070', // required
-  accessToken: '',
-  isDevelopment: true,
-  isOpenSocket: false,  // required
-  lang: 'en', // optional
-  assetsUrl: '/api/v2.1/seadoc/download-image/bf93b0a6-2d02-431a-9de5-9ec7a6f357c9', // required
-  isShowInternalLink: false, // required
-}
+  repoID: '2a77b4e3-0040-4c77-86e2-306cc8f53264',
+  // seafileCollabServer: 'http://127.0.0.1:4000',
+  dirPath: '/',
+  filePath: '/aaa.md',
+  fileName: 'aaa.md',
+};
 
 export default config;
