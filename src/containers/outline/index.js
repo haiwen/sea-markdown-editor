5baac07f0606cc22951005ecd299b0512b626ea2
Guodong Su <stephen.su66@gmail.com>
2024-09-18 11:27:05 +0800

    style:put outline-part-left-alone

--- src/containers/outline/index.js
+++ src/containers/outline/index.js
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 import OutlineItem from './outline-item';
 import { useScrollContext } from '../../hooks/use-scroll-context';
 import { TRANSLATE_NAMESPACE } from '../../constants';
+import EventBus from '../../utils/event-bus';
+import { INTERNAL_EVENTS } from '../../constants/event-types';
 
 import './style.css';
 
@@ -22,6 +24,8 @@ const Outline = ({ editor }) => {
   const scrollRef = useScrollContext();
   const [headerList, setHeaderList] = useState([]);
   const [activeId, setActiveId] = useState('');
+  const [isShown, setIsShown] = useState(false);
+  const [scrollLeft, setScrollLeft] = useState(0);
 
   useEffect(() => {
     const headerList = getHeaderList(editor.children);
@@ -57,14 +61,60 @@ const Outline = ({ editor }) => {
     };
   }, [handleScroll, scrollRef]);
 
+  const toggleShow = useCallback(() => {
+    setIsShown(prevIsShown => {
+      const newIsShown = !prevIsShown;
+      setTimeout(() => {
+        const eventBus = EventBus.getInstance();
+        eventBus.dispatch(INTERNAL_EVENTS.OUTLINE_STATE_CHANGED, newIsShown);
+      }, 0);
+      return newIsShown;
+    });
+  }, []);
+
+  useEffect(() => {
+    const updateScrollLeft = () => {
+      setScrollLeft(window.scrollX); 
+    };
+    window.addEventListener('scroll', updateScrollLeft);
+    return () => {
+      window.removeEventListener('scroll', updateScrollLeft);
+    };
+  }, []);
+
+  
+
   return (
-    <div className="sf-editor-outline">
-      {headerList.length === 0 && (
-        <div className="empty-container">{t('No_outline')}</div>
+    <div className="sf-editor-outline" style={{ left: -scrollLeft }}>
+      {isShown && (
+        <>
+          <div className="sf-editor-outline-header">
+            <h2 className="sf-editor-outline-header_title">{t('Outline')}</h2>
+            <span className="sf-editor-outline-header_close iconfont icon-x" onClick={toggleShow}></span>
+          </div>
+          {headerList.length === 0 && (
+            <div className="empty-container">{t('No_outline')}</div>
+          )}
+          {headerList.length > 0 && (
+            <div className="sf-editor-outline-list-container">
+              {headerList.map((node, index) => {
+              return <OutlineItem key={index} node={node} activeId={activeId} />;
+              })}
+            </div>
+        )}
+        </>
       )}
-      {headerList.length > 0 && headerList.map((node, index) => {
-        return <OutlineItem key={index} node={node} activeId={activeId} />;
-      })}
+      {!isShown && (
+        <>
+          <span
+            id="sf-editor-outline-menu"
+            className="sf-editor-outline-menu sf-edito-tooltip iconfont icon-outline"
+            onClick={toggleShow}
+          >
+            <span className="custom-tooltip">{t('Outline')}</span>
+          </span>
+        </>
+      )}
     </div>
   );
 };
--- src/editors/slate-editor/index.js
+++ src/editors/slate-editor/index.js
@@ -10,12 +10,16 @@ import { focusEditor } from '../../extension/core';
 import { ScrollContext } from '../../hooks/use-scroll-context';
 import useSeafileUtils from '../../hooks/use-insert-image';
 import { isDocumentEmpty, isMac } from '../../utils/common';
+import Outline from "../../containers/outline";
+import { INTERNAL_EVENTS } from '../../constants/event-types';
 
 import './style.css';
 
 const isMacOS = isMac();
 export default function SlateEditor({ value, editorApi, onSave, onContentChanged, isSupportFormula, isSupportInsertSeafileImage, children }) {
   const [slateValue, setSlateValue] = useState(value);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [containerStyle, setContainerStyle] = useState({});
 
   const scrollRef = useRef(null);
   const editor = useMemo(() => withPropsEditor(baseEditor, { editorApi, onSave }), [editorApi, onSave]);
@@ -26,6 +30,23 @@ export default function SlateEditor({ value, editorApi, onSave, onContentChanged
   useSeafileUtils(editor);
 
   const decorate = useHighlight(editor);
+
+  //Adjust article container margin-left value according to isShown of the outline and width of window
+  const handleWindowResize = (newIsShown) => {
+    const rect = scrollRef.current.getBoundingClientRect();
+    const articleRect = document.querySelector('.article')?.getBoundingClientRect();
+    if (newIsShown && (rect.width - articleRect.width) / 2 < 280) {
+      setContainerStyle({ marginLeft: '280px' });
+    } else {
+      setContainerStyle({});
+    }
+  }
+
+  useEffect(() => {
+    const eventBus = EventBus.getInstance();
+    const unsubscribeOutline = eventBus.subscribe(INTERNAL_EVENTS.OUTLINE_STATE_CHANGED, handleWindowResize);
+    return unsubscribeOutline;
+  }, [handleWindowResize]);
 
   const onChange = useCallback((value) => {
     setSlateValue(value);
@@ -89,14 +110,20 @@ export default function SlateEditor({ value, editorApi, onSave, onContentChanged
     }
   }, [editor, focusFirstNode]);
 
+  const onWrapperScroll = useCallback((event) => {
+    const { scrollLeft } = event.target;
+    setScrollLeft(scrollLeft);
+  }, []);
+
   return (
     <div className='sf-slate-editor-container'>
       <Toolbar editor={editor} isRichEditor={true} isSupportFormula={isSupportFormula} isSupportInsertSeafileImage={isSupportInsertSeafileImage} />
       <div className='sf-slate-editor-content' onClick={onEditorClick}>
         <ScrollContext.Provider value={{ scrollRef }}>
           <Slate editor={editor} initialValue={slateValue} onChange={onChange}>
-            <div ref={scrollRef} className={`sf-slate-scroll-container ${isMacOS ? '' : 'isWin'}`}>
-              <div className='sf-slate-article-container'>
+            <div ref={scrollRef} className={`sf-slate-scroll-container ${isMacOS ? '' : 'isWin'}`} onScroll={onWrapperScroll}>
+              <Outline editor={editor} />
+              <div className='sf-slate-article-container' style={containerStyle}>
                 <div className='article'>
                   <SetNodeToDecorations />
                   <Editable
@@ -108,8 +135,8 @@ export default function SlateEditor({ value, editorApi, onSave, onContentChanged
                   />
                 </div>
               </div>
+              <EditorHelp children={children}></EditorHelp>
             </div>
-            <EditorHelp children={children}></EditorHelp>
           </Slate>
         </ScrollContext.Provider>
       </div>
