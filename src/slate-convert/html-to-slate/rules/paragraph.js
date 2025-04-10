3ca32c07e1f35a53db1f83891431924c0d36aa5c
杨顺强 <978987373@qq.com>
2025-04-10 16:28:46 +0800

    fix code bug (#234)
    Co-authored-by: 小强 <shuntian@xiaoqiangdeMacBook-Pro.local>

--- src/slate-convert/html-to-slate/rules/paragraph.js
+++ src/slate-convert/html-to-slate/rules/paragraph.js
@@ -4,6 +4,18 @@ import { PARAGRAPH } from '../constants';
 const paragraphRule = (element, parseChild) => {
   const { nodeName, childNodes } = element;
   if (nodeName === 'P' && element.parentElement.nodeName !== 'LI') {
+    if (childNodes.length === 0) {
+      return {
+        id: slugid.nice(),
+        type: PARAGRAPH,
+        children: [
+          {
+            id: slugid.nice(),
+            text: '',
+          }
+        ]
+      };
+    }
     return {
       id: slugid.nice(),
       type: PARAGRAPH,
