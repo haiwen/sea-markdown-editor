f27d1c51dd72e7a57028a0f6677fb8c92f79e898
杨国璇 <ygx@MacBookPro.lan>
2025-07-22 15:53:43 +0800

    feat: clear article

--- site/pages/long-text-inline-page.js
+++ site/pages/long-text-inline-page.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { LongTextInlineEditor } from '@seafile/seafile-editor';
+import { Button } from 'reactstrap';
+import { LongTextInlineEditor, EventBus, EXTERNAL_EVENTS } from '@seafile/seafile-editor';
 import editorApi from '../api';
 
 import '../assets/css/longtext-page.css';
@@ -23,17 +24,24 @@ const LongTextInlinePage = () => {
     setFileValue(text);
   }, []);
 
+  const clearContent = useCallback(() => {
+    const eventBus = EventBus.getInstance();
+    eventBus.dispatch(EXTERNAL_EVENTS.CLEAR_ARTICLE);
+  }, []);
+
   if (isFetching) {
     return '';
   }
 
   return (
     <div className='long-text-page'>
+      <Button color="primary" className="position-absolute" style={{ right: 0 }} onClick={clearContent} >{'清空内容'}</Button>
       <div className='editor-wrapper'>
         <div className='preview-container' style={{ padding: '30px 8px', width: 600 }}>
           <LongTextInlineEditor
+            isAlwaysEnableEdit={true}
             lang={'zh-cn'}
-            autoSave={true}
+            autoSave={false}
             saveDelay={6000}
             isCheckBrowser={true}
             headerName={'Edit cell value'}
