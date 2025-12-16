e0f095ac1d1bf908c105b676c195633e363a2a43
Guodong Su <stephen.su66@gmail.com>
2025-12-16 10:14:04 +0800

    feat: update translation

--- src/extension/plugins/image/render-element/image-previewer.js
+++ src/extension/plugins/image/render-element/image-previewer.js
@@ -3,6 +3,7 @@ import { useSlateStatic } from 'slate-react';
 import PropTypes from 'prop-types';
 import Lightbox from '@seafile/react-image-lightbox';
 import { getImagesUrlList } from '../helper';
+import { useTranslation } from 'react-i18next';
 
 import '@seafile/react-image-lightbox/style.css';
 import './style.css';
@@ -10,6 +11,7 @@ import './style.css';
 const ImagePreviewer = ({ imgUrl, toggleImagePreviewer }) => {
   const editor = useSlateStatic();
   let images = getImagesUrlList(editor.children);
+  const t = useTranslation(TRANSLATE_NAMESPACE);
   const [imageIndex, setImageIndex] = useState(images.findIndex((item) => item === imgUrl));
   const mainSrc = images[imageIndex];
 
@@ -44,6 +46,8 @@ const ImagePreviewer = ({ imgUrl, toggleImagePreviewer }) => {
           zIndex: 1071
         }
       }}
+      zoomInTip={t('Enlarge: ⌘ + Wheel')}
+      zoomOutTip={t('Shrink: ⌘ + Wheel')}
     />
   );
 };
