d595d4dcfb0abffa50522f3e3aa1e1f40bdb3c0f
Guodong Su <stephen.su66@gmail.com>
2026-03-20 18:00:42 +0800

    feat: optimize code

--- src/extension/plugins/divider/plugin.js
+++ src/extension/plugins/divider/plugin.js
@@ -1,7 +1,11 @@
+import { Editor, Element, Node, Path, Range, Transforms } from 'slate';
 import { ELementTypes } from '../../constants';
+import { generateEmptyElement } from '../../core';
+
+const HR_PATTERNS = ['---', '***', '___'];
 
 const withDivider = (editor) => {
-  const { isVoid } = editor;
+  const { isVoid, insertText } = editor;
   const newEditor = editor;
 
   newEditor.isVoid = (element) => {
@@ -9,6 +13,43 @@ const withDivider = (editor) => {
 
     if (type === ELementTypes.DIVIDER) return true;
     return isVoid(element);
+  };
+
+  newEditor.insertText = (text) => {
+    const { selection } = newEditor;
+    if (!selection && Range.isCollapsed(selection)) {
+      insertText(text);
+      return;
+    }
+
+    const [blockEntry] = Editor.nodes(newEditor, {
+      at: selection,
+      match: (n) => Element.isElement(n) && n.type === ELementTypes.PARAGRAPH,
+    });
+
+    if (!blockEntry) {
+      insertText(text);
+      return;
+    }
+
+    const [blockNode, blockPath] = blockEntry;
+    const blockText = Node.string(blockNode);
+
+    const nextText = blockText + text;
+
+    if(HR_PATTERNS.includes(nextText)) {
+      Editor.withoutNormalizing(newEditor, () => {
+        Transforms.removeNodes(newEditor, { at: blockPath });
+        Transforms.insertNodes(newEditor, generateEmptyElement(ELementTypes.DIVIDER), { at: blockPath });
+
+        const nextPath = Path.next(blockPath);
+        Transforms.insertNodes(newEditor, generateEmptyElement(ELementTypes.PARAGRAPH), { at: nextPath });
+        Transforms.select(newEditor, Editor.start(newEditor, nextPath));
+      });
+      return;
+    }
+
+    return insertText(text);
   };
 
   return newEditor;
